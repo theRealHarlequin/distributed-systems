e343ac57e9f6bca04257be055a1cadff55f7ac65
Lenovo Harlequin <the_harlequin0815@protonmail.com>
2025-04-14 20:19:56 +0100

    add examples

--- run_examples.py
+++ run_examples.py
@@ -8,19 +8,29 @@ from logger import Logger
 current_dir = os.path.dirname(os.path.abspath(__file__))
 log = Logger()
 
-_scripts = ["proxy.py", "pub.py", "sub.py"]
 _chapter = "_99_examples"
 _area = "zeromq_protobuf"
-_folder = "pub_sub_proxy"
+
+# pub_sub
+_scripts = ["pub.py", "sub.py"]
+_param = [["1"], ["1", "1"]]
+_folder = "pub_sub"
+
+# pub_sub_proxy
+#_scripts = ["proxy.py", "pub.py", "sub.py"]
+#_param = [["1"], ["1", "1"]]
+#_folder = "pub_sub_proxy"
+
+
 processes = []
 log.log(msg="init_env", level=logging.INFO)
 
 # Start all scripts
-for script in _scripts:
-    script_path = os.path.join(current_dir, _chapter, _area, _folder, script)  # Ensure correct path
+for i in range(0,len(_scripts) - 1):
+    script_path = os.path.join(current_dir, _chapter, _area, _folder, _scripts[i])  # Ensure correct path
 
-    p = subprocess.Popen(["python", script_path])
-    log.log(msg=f"start_process of {script} at {script_path}", level=logging.INFO)
+    p = subprocess.Popen(["python", script_path] + _param[i])
+    log.log(msg=f"start_process of {_scripts[i]} at {script_path}", level=logging.INFO)
     processes.append(p)
 
 # Wait for 3 seconds
