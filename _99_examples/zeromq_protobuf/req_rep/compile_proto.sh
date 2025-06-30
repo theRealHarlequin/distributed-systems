936a9a2586eda22b73783fbd4b86b615b03e0519
Lenovo Harlequin <the_harlequin0815@protonmail.com>
2025-06-30 18:20:01 +0200

    cleanup

--- _99_examples/zeromq_protobuf/req_rep/compile_proto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for f in $(ls *.proto); do
-    "C:\work\study\lab_verteilte_Systme\protoc\bin\protoc.exe" --proto_path=. ./$f --python_out=.
-done
-
-sleep 5
--- _99_examples/zeromq_protobuf/req_rep/startup.sh
+++
@@ -1,9 +0,0 @@
-. ../venv/bin/activate
-
-python server.py &
-PID1=$!
-
-sleep 2
-
-python client.py
-kill $PID1
